74886a585a840bf66b918b267f12eb33da5d0f63
Gaurav Kumar Tailor <gauravtailorthoi@gmail.com>
2021-09-12 10:38:58 +0530

    final

--- unk.py
+++
@@ -1,7 +0,0 @@
-def sockMerchant(n, ar)
-    #converting list into set so no repetely count in the loop.
-    s = set(ar)
-    c=0
-    for item in s:
-        c = ar.count(item)//2 + c
-    print(c)
